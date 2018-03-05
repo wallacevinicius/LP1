9c48ef12953f1092a65c19b2eb9954076a2f8829
Wallace Vinícius <32179427+wallacevinicius@users.noreply.github.com>
2018-03-05 18:26:54 -0300

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -4,18 +4,14 @@
 
 int main(){
     int i;
-    char word[100], drow[100], aux[100];
+    char word[100], drow[100];
 
     printf("Type your word: ");
     fgets(word, 100, stdin);
 
-    strcpy(aux, word);
-
     //Take away the '\n'
     if(word[strlen(word)-1] == '\n')
         word[strlen(word)-1] = '\0';
-    if(aux[strlen(aux)-1] == '\n')
-        aux[strlen(aux)-1] = '\0';
 
     //Make all word become uppercase
     for(i = 0; word[i]; i++){
@@ -29,9 +25,9 @@ int main(){
 
     //If strcmp returns 0 the word is a palindrome
     if(!strcmp(drow, word))
-        printf("\n\"%s\" is a palindrome", aux);
+        printf("\n\"%s\" is a palindrome", word);
     else
-        printf("\n\"%s\" isn't a palindrome", aux);
+        printf("\n\"%s\" isn't a palindrome", word);
 
     return 0;
 }
