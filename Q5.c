234af96f2d9e1a1378086507c20f04babfde0595
Wallace Vinícius <32179427+wallacevinicius@users.noreply.github.com>
2018-03-06 17:26:16 -0300

    Add files via upload

--- Q5.c
+++ Q5.c
@@ -1,31 +1,93 @@
 #include <stdio.h>
-#define NUMBERPEOPLE 10
+#include <string.h>
+#define NUMBERPEOPLE 5
+#define MAXHEIGHT 10
+
+void BSHeight(float alt[], char gnr[]){
+    int i;
+    float biggestHeight = 0, smallerHeight = MAXHEIGHT;
+    char biggestGenre, smallerGenre;
+
+    for(i = 0; i < NUMBERPEOPLE; i++){
+        if(alt[i] > biggestHeight){
+            biggestHeight = alt[i];
+            biggestGenre = gnr[i];
+        }
+        if(alt[i] < smallerHeight){
+            smallerHeight = alt[i];
+            smallerGenre = gnr[i];
+        }
+    }
+
+    printf("\nThe biggest height is %.2lf from a ", biggestHeight);
+    if(biggestGenre == 'M' || biggestGenre == 'm')
+        printf("male person");
+    else if(biggestGenre == 'F' || biggestGenre == 'f')
+        printf("female person");
+
+    printf("\nThe smaller height is %.2lf from a ", smallerHeight);
+    if(smallerGenre == 'M' || smallerGenre == 'm')
+        printf("male person\n");
+    else if(smallerGenre == 'F' || smallerGenre == 'f')
+        printf("female person\n");
+}
+void Average(float alt[], char gnr[]){
+    int i, quantMale = 0, quantFemale = 0;
+    float sumMale = 0, sumFemale = 0, averageMale, averageFemale;
+
+    for(i = 0; i < NUMBERPEOPLE; i++){
+        if(gnr[i] == 'M' || gnr[i] == 'm'){
+            quantMale++;
+            sumMale += alt[i];
+        }else if(gnr[i] == 'F' || gnr[i] == 'f'){
+            quantFemale++;
+            sumFemale += alt[i];
+        }
+    }
+
+    averageMale     = sumMale/quantMale;
+    averageFemale   = sumFemale/quantFemale;
+
+    printf("\nThe average of male height is %.2lf\n", averageMale);
+    printf("The average of female height is %.2lf\n", averageFemale);
+
+}
+void QuantGenre(float alt[], char gnr[]){
+    int i, quantMale = 0, quantFemale = 0;
+
+    for(i = 0; i < NUMBERPEOPLE; i++){
+        if(gnr[i] == 'M' || gnr[i] == 'm'){
+            quantMale++;
+        }else if(gnr[i] == 'F' || gnr[i] == 'f'){
+            quantFemale++;
+        }
+    }
+
+    printf("\nIt has %d male person\n", quantMale);
+    printf("It has %d female person", quantFemale);
+}
 
 int main(){
     int i;
-    float height[NUMBERPEOPLE], biggestHeight, smallerHeight;
-    char genre[NUMBERPEOPLE], biggestGenre, smallerGenre;
+    float height[NUMBERPEOPLE];
+    char genre[NUMBERPEOPLE];
 
     for(i = 0; i < NUMBERPEOPLE; i++){
         printf("Type the GENRE: ");
-        scanf("%*c", genre[i]);
+        scanf("%c", &genre[i]);
 
         printf("Type the HEIGHT: ");
-        scanf("%f", height[i]);
+        scanf("%f%*c", &height[i]);
     }
 
-    for(i = 0; i < NUMBERPEOPLE; i++){
-        if(height[i] > height[i+1]){
-            biggestHeight = height[i];
-            biggestGenre = genre[i];
-        }
-        if(height[i] < height[i+1]){
-            smallerHeight = height[i];
-            smallerGenre = genre[i];
-        }
-    }
+    //Calculate the biggest and smaller height
+    BSHeight(height, genre);
 
-    printf("maior = %.2f", biggestHeight);
+    //Calculate the average height of each gender
+    Average(height, genre);
+
+    //Calculate the number of people of each gender
+    QuantGenre(height, genre);
 
     return 0;
 }
