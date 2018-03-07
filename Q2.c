21566c15fb6d3383b99f5db1b03eefb38e5d75aa
Wallace Vinícius <32179427+wallacevinicius@users.noreply.github.com>
2018-03-07 14:28:58 -0300

    Add files via upload

--- Q2.c
+++ Q2.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main(){
+    int numberBefore, numberAfter, sum, quant, i;
+
+    printf("Type the numbers amount: ");
+    scanf("%d", &quant);
+
+    numberBefore = 0;
+    numberAfter = 1;
+
+    for(i = 0; i < quant; i++){
+        if(i == 0)
+            printf("%d ", numberAfter);
+
+        sum = numberBefore + numberAfter;
+        numberBefore = numberAfter;
+        numberAfter = sum;
+
+        printf("%d ", sum);
+    }
+
 
     return 0;
 }
